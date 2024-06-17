4f0abb08e1a8925470e9c0a0d658e1c69f031666
balogun michael <balogunmaleek2001@gmail.com>
2024-06-17 12:54:20 -1100

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,13 @@
 
 module.exports = {
     theme: {
-      extend: {
-        colors: {
-          // Add any custom colors you might need
-          'Off white': 'hsl(0, 0%, 94%)', 'White': 'hsl(0, 0%, 100%)',
-          'Smokey grey': 'hsl(0, 1%, 44%)',
-'Off black':'hsl(0, 0%, 8%)',
-        },
-      },
-    },
-    variants: {},
-    plugins: [
-      function({ addUtilities }) {
-        addUtilities({
-          '.text-outline': {
-            '-webkit-text-stroke-width': '1px',
-            '-webkit-text-stroke-color': 'currentColor',
-            'color': 'transparent',
-          },
-        }, ['responsive', 'hover']);
-      },
-    ],
+        extend: {
+      colors: {
+        // Configure your color palette here
+'Off white': 'hsl(0, 0%, 94%)', 'White': 'hsl(0, 0%, 100%)',
+        'Smokey grey': 'hsl(0, 1%, 44%)', 'Purple': 'hsl(259, 100%, 65%)',
+        "Off black": 'hsl(0, 0%, 8%)'
+      }
+    }
+}
   }
-  
